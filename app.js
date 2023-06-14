12c890e05d194e4eb0c5d49e2207578786c46025
MirandaMG20 <guadamiran@gmail.com>
2023-06-14 11:34:54 -0400

    message

--- app.js
+++ app.js
@@ -2,8 +2,24 @@
 const adventurer = {
     name: "Miranda",
     hitPoint: 10;
-    belongings: ["staff", "potions", "medicine"]
+    belongings: ["staff", "potions", "medicine"],
+    companion: {
+        name: "Brownie",
+        type: "Dog"
+    }
 }
+
+console.log(`My companion's name is ${adventurer.companion.name}`);
 
 // access the values in the array
 console.log(adventurer.belongings)
+
+// access the first belonging
+console.log(`first belonging ${adventurer.belongings[0]}`);
+
+// iterate over an array inside of an object
+for(let i = 0; i < adventurer.belongings.length; i++){
+    console.log(adventurer.belongings[i])
+}
+
+// #1 OBJ within OBJ

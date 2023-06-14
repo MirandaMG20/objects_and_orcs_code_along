f7ad978bc4ec4cbcddbb098037bb5ed67d359287
MirandaMG20 <guadamiran@gmail.com>
2023-06-14 12:25:23 -0400

    update

--- app.js
+++ app.js
@@ -54,16 +54,68 @@
 // foo.doSomething(); // 'I did something!'
 
 // An array of arrays
-const foo = [
-    [1,2,3],
-    ["4", "5", "6"],
-    [7, 8, 9],
-    () => {
-        console.log("I am a function inside an array")
-    }
-]
-console.log(foo[0]) // []
-console.log(foo[0][1]) // 2
+// const foo = [
+//     [1, 2, 3],
+//     ["4", "5", "6"],
+//     [7, 8, 9],
+//     () => {
+//         console.log("I am a function inside an array")
+//     }
+// ]
+// console.log(foo[0]) // []
+// console.log(foo[0][1]) // 2
 
-// Storing functions in an array
-console.log(foo[3]());
+// // Storing functions in an array
+// console.log(foo[3]())
+
+// CREATING A CLASS
+class Character {
+    constructor(name, age, eyes, hair, lovesCats = true, lovesDogs) {
+        this.legs = 2;
+        this.arms = 2;
+        this.name = name;
+        this.age = age;
+        this.eyes = eyes;
+        this.hair = hair;
+        this.lovesCats = lovesCats;
+        this.loveDogs = lovesDogs || true; // default to true
+    }
+    setHairColor (HairColor) {
+        this.hair = HairColor;
+    }
+    //method1
+    greet(otherCharacter) {
+        console.log(`hello ${otherCharacter}`);
+    }
+    //method2
+    smite() {
+        console.log(`I smite thee you vile person!`)
+    }
+    static greet() {
+        console.log(`hi`);
+    }
+}
+const alexander = new Character();
+const brock = new Character();
+
+// const Miranda = new Character();
+// const Brownie = new Character();
+
+// Miranda.greet();
+// Brownie.greet();
+
+// console.log(Miranda);
+// console.log(typeof Miranda);
+
+
+// alexander.greet("Brock");
+// brock.greet("Alexander");
+// alexander.smite();
+
+// const Miranda = new Character();
+// Miranda.eyes = "brown";
+// console.log(Miranda);
+
+// name. age, eyes, hair, cats, dogs
+const Miranda = new Character("Miranda", "150", "green", "Silver")
+Miranda.setHairColor('red')

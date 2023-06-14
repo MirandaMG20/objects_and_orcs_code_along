0d7d31a0aff51df8259ced4fa9a661ac4d812ef3
MirandaMG20 <guadamiran@gmail.com>
2023-06-14 11:25:31 -0400

    message

---
+++ app.js
@@ -0,0 +1,9 @@
+// Use array inside an object
+const adventurer = {
+    name: "Miranda",
+    hitPoint: 10;
+    belongings: ["staff", "potions", "medicine"]
+}
+
+// access the values in the array
+console.log(adventurer.belongings)
